2398d6bf766e89a5714b70f4243b386266b45dd4
RubenTomasPita <111887826+RubenTomasPita@users.noreply.github.com>
2023-09-19 12:09:13 -0400

    Initial commit

---
+++ agregador/src/App.js
@@ -0,0 +1,11 @@
+//Esto sirve para importar los componentes
+import TodoApp from "../src/componentes/todo";
+
+//Aqui llamamos el componente
+function App() {
+  return (
+    <TodoApp/>
+  );
+}
+
+export default App;
